8289fb700a1b6d0bd399a061aeb44d8ff862b44d
Jungho Bang <me@bangtoven.com>
2017-01-27 17:37:11 -0500

    experiment on grid.

--- js/main.grid.js
+++ js/main.grid.js
@@ -220,6 +220,13 @@ function onPlayerStateChange(event) {
   }
 }
 
+var widgets = [
+    {x: 0, y: 0, width: 2, height: 2},
+    {x: 2, y: 0, width: 4, height: 2},
+    {x: 6, y: 0, width: 2, height: 4},
+    {x: 1, y: 2, width: 4, height: 2}
+];
+
 // run the function when the document is ready
 $(document).ready(function () {
   var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -272,7 +279,83 @@ $(document).ready(function () {
     editor.addKeyMap(map);
 
 	$("#youtube-result").hide();
+
+
+    var counter = 0;
+
+    ko.components.register('dashboard-grid', {
+        viewModel: {
+            createViewModel: function (controller, componentInfo) {
+                var ViewModel = function (controller, componentInfo) {
+                    var grid = null;
+
+                    this.widgets = controller.widgets;
+
+                    this.afterAddWidget = function (items) {
+                        if (grid == null) {
+                            grid = $(componentInfo.element).find('.grid-stack').gridstack({
+                                auto: false
+                            }).data('gridstack');
+                        }
+
+                        var item = _.find(items, function (i) { return i.nodeType == 1 });
+                        grid.addWidget(item);
+                        ko.utils.domNodeDisposal.addDisposeCallback(item, function () {
+                            grid.removeWidget(item);
+                        });
+
+                        counter++;
+                        item.getElementsByClassName("yt-cell")[0].id = "yt-cell-" + counter;
+
+                        var player = new YT.Player("yt-cell-" + counter, {
+                            height: 100,
+                            width:  100,
+                            videoId: "5y88CkstcWc",
+                            suggestedQuality:"small"
+                        });
+                    };
+                };
+
+                return new ViewModel(controller, componentInfo);
+            }
+        },
+        template:
+            [
+                '<div class="grid-stack" data-bind="foreach: {data: widgets, afterRender: afterAddWidget}">',
+                '   <div class="grid-stack-item" data-bind="attr: {\'data-gs-x\': $data.x, \'data-gs-y\': $data.y, \'data-gs-width\': $data.width, \'data-gs-height\': $data.height, \'data-gs-auto-position\': $data.auto_position}">',
+                '       <div class="grid-stack-item-content">',
+                '			<button data-bind="click: $root.deleteWidget">Delete me</button><br>',
+                '			<div class="yt-cell"></div>',
+                '		</div>',
+                '   </div>',
+                '</div> '
+            ].join('')
+    });
+
+    var Controller = function (widgets) {
+        var self = this;
+
+        this.widgets = ko.observableArray(widgets);
+    };
+
+    var controller = new Controller(widgets);
+    ko.applyBindings(controller);
 });
+
+function deleteWidget (item) {
+    self.widgets.remove(item);
+    return false;
+};
+
+function add() {
+    this.widgets.push({
+        x: 0,
+        y: 0,
+        width: Math.floor(1 + 3 * Math.random()),
+        height: Math.floor(1 + 3 * Math.random()),
+        auto_position: true
+    });
+}
 
 function addGrid(addRow,addCol,id){
   initialLoading = true;
